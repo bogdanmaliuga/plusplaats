c0d4a1d297cd9d4f0997e343ef8ea7e2a80cf32a
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2017-04-28 12:05:55 +0300

    add search-page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($) {
-	 $(document).scroll(function() {
+    $(document).scroll(function() {
         var $nav = $(".navbar-fixed-top");
         console.log($(this).scrollTop());
         console.log($nav.height());
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
-	  var owl = $('#owl1');
+    var owl = $('#owl1');
     owl.owlCarousel({
         loop: true,
         margin: 38.25,
@@ -21,23 +21,128 @@ jQuery(document).ready(function($) {
                 items: 2,
                 pagination: false
             },
-            667:{
-                items:3,
-                pagination:false
+            667: {
+                items: 3,
+                pagination: false
             },
             768: {
-                items:2
+                items: 2
             },
-            1024:{
-                items:3
+            1024: {
+                items: 3
             },
-            1300:{
-                items:4
+            1300: {
+                items: 4
             }
 
 
 
 
+        }
+    });
+    var owlSearch = $('#owl-search-1');
+    owlSearch.owlCarousel({
+        loop: true,
+        margin: 38.25,
+        mouseDrag: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                pagination: false,
+                margin: 27,
+                stagePadding: 54
+            },
+            540: {
+                items: 2,
+                pagination: false
+            },
+            667: {
+                items: 3,
+                pagination: false
+            },
+            768: {
+                items: 2,
+                pagination: false
+            },
+            1024: {
+                items: 3,
+                pagination: false
+            },
+            1300: {
+                items: 3,
+                pagination: false
+            }
+        }
+    });
+    var owlSearch2 = $('#owl-search-2');
+    owlSearch2.owlCarousel({
+        loop: true,
+        margin: 38.25,
+        mouseDrag: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                pagination: false,
+                margin: 27,
+                stagePadding: 54
+            },
+            540: {
+                items: 2,
+                pagination: false
+            },
+            667: {
+                items: 3,
+                pagination: false
+            },
+            768: {
+                items: 2,
+                pagination: false
+            },
+            1024: {
+                items: 3,
+                pagination: false
+            },
+            1300: {
+                items: 3,
+                pagination: false
+            }
+        }
+    });
+    var owlSearch3 = $('#owl-search-3');
+    owlSearch3.owlCarousel({
+        loop: true,
+        margin: 38.25,
+        mouseDrag: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                pagination: false,
+                margin: 27,
+                stagePadding: 54
+            },
+            540: {
+                items: 2,
+                pagination: false
+            },
+            667: {
+                items: 3,
+                pagination: false
+            },
+            768: {
+                items: 2,
+                pagination: false
+            },
+            1024: {
+                items: 3,
+                pagination: false
+            },
+            1300: {
+                items: 3,
+                pagination: false
+            }
         }
     });
 });
